6a3a69c5ec8adb52b70806826e2533e1882e55d3
rerohierophant <3044769525@qq.com>
2024-09-04 15:14:48 +0800

    new_api

--- FiveMusic/asgi.py
+++ FiveMusic/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for EmoMusic project.
+ASGI config for FiveMusic project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EmoMusic.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FiveMusic.settings")
 
 application = get_asgi_application()
--- FiveMusic/wsgi.py
+++ FiveMusic/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for EmoMusic project.
+WSGI config for FiveMusic project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EmoMusic.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FiveMusic.settings")
 
 application = get_wsgi_application()
